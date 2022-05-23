c89f75831b9b07a9fc39e47800e438f70b63e1ff
Jan Kirenz <48259550+kirenz@users.noreply.github.com>
2022-05-23 12:09:00 +0200

    Update my_airflow_dag.py

--- my_airflow_dag.py
+++ my_airflow_dag.py
@@ -15,8 +15,8 @@ from datetime import datetime, timedelta
 # Some convenience functions
 from textwrap import dedent
 
-
 #---------------------------------------
+
 
 # DEFAULT DAG ARGUMENTS
 
@@ -91,10 +91,25 @@ with DAG(
     
     # EXAMPLE OF TEMPLATING WITH JINJA 
 
+    # The templated_command contains code logic in {% %} blocks, 
+    # references parameters like {{ ds }}   #[ds = today's "date stamp"]
+    # and calls a function as in {{ macros.ds_add(ds, 7)}}
+
+    templated_command = dedent(
+        """
+    {% for i in range(5) %}
+        echo "{{ ds }}"
+        echo "{{ macros.ds_add(ds, 7)}}"
+    {% endfor %}
+    """
+    )
+
+    # To see examples of more templates, visit https://airflow.apache.org/docs/apache-airflow/stable/templates-ref.html#templates-ref
+
     t3 = BashOperator(
         task_id='task_templated',
         depends_on_past=False,
-        bash_command='templated_command.sh',
+        bash_command= templated_command,
     )
 
     #----------------
