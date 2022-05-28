3cd046afa9c50a6bf9d08d32b1ea39e59ef98e18
Jan Kirenz <48259550+kirenz@users.noreply.github.com>
2022-05-28 12:25:52 +0200

    Update model.py

--- dag_reg_example/model.py
+++ dag_reg_example/model.py
@@ -15,13 +15,14 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from joblib import dump
 
+from my_path import home_path, airflow_path
+
 #------------------------------------------------------
 # Step 1) Import data 
-df = pd.read_csv("df_prepped.csv")
+df = pd.read_csv(home_path + airflow_path + "df_prepped.csv")
 
 #------------------------------------------------------
 # Step 2) Prepare data
-
 # Select features for simple regression
 X = df[['gdp_per_capita']]
 # Create response
@@ -31,7 +32,6 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 
 #------------------------------------------------------
 # Step 3) Fit model
-
 # Select regression model
 reg = LinearRegression()
 # Train the model
@@ -39,4 +39,4 @@ reg.fit(X_train, y_train)
 
 #------------------------------------------------------
 # Step 4) Store model
-dump(reg, 'my_linear_model.joblib')
+dump(reg, home_path + airflow_path + 'my_linear_model.joblib')
