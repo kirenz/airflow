a6257e1d7fb33859cb944f6a757ecc0db343d877
Jan Kirenz <48259550+kirenz@users.noreply.github.com>
2022-05-28 12:46:03 +0200

    Update data.py

--- dag_reg_example/data.py
+++ dag_reg_example/data.py
@@ -6,19 +6,22 @@ Step 2) Make some data corrections
 Step 3) Save data as csv to local folder
 
 """
+#------------------------------------------------------
+# Setup
 import pandas as pd
 from pathlib import Path
 
+from my_path import home_path, airflow_path
+
+#------------------------------------------------------
 # Step 1: Import data from GitHub
 LINK = "https://raw.githubusercontent.com/kirenz/datasets/master/oecd_gdp.csv"
 df = pd.read_csv(LINK)
 
+#------------------------------------------------------
 # Step 2: Change column names 
 df.columns = df.columns.str.lower().str.replace(' ', '_')
+#------------------------------------------------------
 
 # Step 3: Save data to current working directory
-
-home = str(Path.home())
-airflow = "/airflow/dags/dag_reg_example/"
-
-df.to_csv(home + airflow + 'df_prepped.csv', index=False)
+df.to_csv(home_path + airflow_path + 'df_prepped.csv', index=False)
