3727e11a8e14366fb50b90c533bdba0d83683f95
Jan Kirenz <48259550+kirenz@users.noreply.github.com>
2022-05-28 14:29:51 +0200

    Update prediction.py

--- dag_reg_example/prediction.py
+++ dag_reg_example/prediction.py
@@ -10,7 +10,6 @@ Step 4) Store prediction
 #------------------------------------------------------
 # Setup
 import pandas as pd
-from sklearn.linear_model import LinearRegression
 from joblib import load
 
 from my_path import home_path, airflow_path
@@ -35,9 +34,9 @@ my_prediction = reg.predict(X_new)
 # Step 4) Save prediction
 
 # Save prediction as dataframe 
-df_prediction = pd.Dataframe({"pred": my_prediction})
+df_prediction = pd.DataFrame({"pred": my_prediction})
 
 # Store predictions as csv
-df.prediction.to_csv(home_path + airflow_path + "my_prediction.csv")
+df_prediction.to_csv(home_path + airflow_path + "my_prediction.csv", echo=False)
 
 #------------------------------------------------------
